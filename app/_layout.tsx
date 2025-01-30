f914e37d253dfea9a3d26bbef53eb5af06b0bfbc
LuisLDA <luisldacr@gmail.com>
2025-01-29 19:24:27 -0500

    feat: :sparkles: Eliminar archivos obsoletos y agregar la estructura básica de la aplicación

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
 // Import your global CSS file
 import "../global.css";
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <View className={"p-4 bg-black  h-screen"}>
+      <Text className={"font-extrabold text-5xl text-white"}>
+        Header
+      </Text>
+
+      <Slot />
+
+      <Text>
+        Footer
+      </Text>
+
+
+
+    </View>
+  )
 }
+
+export default RootLayout
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CalculatorApp = () => {
+    return (
+        <View>
+            <Text className='font-bold text-red-500'>IA App</Text>
+        </View>
+    )
+}
+
+export default CalculatorApp
